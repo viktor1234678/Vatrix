1f28ed0fad602d6a5a1689c6df97606bcbb20b3b
viktor1234678 <viktorvondombrowski@gmail.com>
2021-05-03 19:43:46 +0200

    Create main.cpp

---
+++ examples-api-use/matryx-gl-master/src/main.cpp
@@ -0,0 +1,38 @@
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <ratio>
+#include <thread>
+
+#include "Canvas.hpp"
+#include "FrameTimer.hpp"
+#include "Matrix.hpp"
+#include "MatrixContext.hpp"
+#include "scenes/WatermelonPlasmaScene.hpp"
+#include "scenes/WaveScene.hpp"
+
+constexpr double targetFps = 25;
+constexpr std::chrono::duration<double> targetDurationPerFrame(1.0 / targetFps);
+
+int main(int argc, char *argv[]) {
+  auto matrix = std::make_shared<matryx::Matrix>("ipc:///tmp/matrix_0", 64, 32);
+  matrix->connect();
+
+  auto canvas =
+      std::make_shared<matryx::Canvas>(matrix->getWidth(), matrix->getHeight());
+  auto matrixCtx = std::make_shared<matryx::MatrixContext>(matrix, canvas);
+
+  matryx::scenes::WaveScene scene(matrixCtx);
+  scene.setup();
+
+  matryx::FrameTimer frameTimer;
+  matryx::FrameTimer paceTimer;
+
+  while (true) {
+    auto frameTime = frameTimer.tick();
+    scene.tick(frameTime);
+    std::this_thread::sleep_until(frameTime.now + targetDurationPerFrame);
+  }
+
+  return 0;
+}
