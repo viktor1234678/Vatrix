3d385016e5ad7ecaffb3bbdf8c8070beba23a69e
viktor1234678 <viktorvondombrowski@gmail.com>
2021-05-05 23:36:16 +0200

    Update matrix-rain.cs

--- bindings/c#/examples/matrix-rain.cs
+++ bindings/c#/examples/matrix-rain.cs
@@ -45,7 +45,7 @@ namespace matrix_rain
 
                 foreach (var point in points)
                 {
-                    canvas.SetPixel(point.x, point.y, new Color(66, 66, 66));
+                    canvas.SetPixel(rnd, 0, new Color(66, 66, 66));
                     if (!point.recycled)
                     {
                         point.y++;
