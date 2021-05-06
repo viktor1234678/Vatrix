668249ff98a5aa6bb74c4a4a80ebb27b00625224
viktor1234678 <viktorvondombrowski@gmail.com>
2021-05-06 17:13:25 +0200

    Create Pokemon.cs

---
+++ bindings/c#/examples/Pokemon.cs
@@ -0,0 +1,91 @@
+﻿using rpi_rgb_led_matrix_sharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Pokemon
+{
+    class Program
+    {
+        const int MAX_HEIGHT = 1;
+        const int COLOR_STEP = 12;
+        const int FRAME_STEP = 1;
+
+        static int Main(string[] args)
+        {
+
+            var matrix = new RGBLedMatrix(new RGBLedMatrixOptions { ChainLength = 2 });
+            var canvas = matrix.CreateOffscreenCanvas();
+            var rnd = new Random();
+            var points = new List<Point>();
+            var recycled = new Stack<Point>();
+            int frame = 0;
+            var stopwatch = new Stopwatch();
+
+            while (!Console.KeyAvailable) {
+                stopwatch.Restart();
+
+                frame++;
+
+                if (frame % FRAME_STEP == 0)
+                {
+                    if (recycled.Count == 0)
+                        points.Add(new Point(rnd.Next(0, canvas.Width - 1), 0));
+                    else
+                    {
+                        var point = recycled.Pop();
+                        point.x = rnd.Next(0, canvas.Width - 1);
+                        point.y = 0;
+                        point.recycled = false;
+                    }
+                }
+
+                canvas.Clear();
+
+                foreach (var point in points)
+                {
+                    canvas.SetPixel(0, 0, new Color(66, 66, 66));
+                    if (!point.recycled)
+                    {
+                        point.y++;
+
+                        if (point.y - MAX_HEIGHT > canvas.Height)
+                        {
+                            point.recycled = true;
+                            recycled.Push(point);
+                        }
+
+                        for (var i=0; i< MAX_HEIGHT; i++)
+                        {
+                            canvas.SetPixel(point.x, point.y - i, new Color(3, 252 - i * COLOR_STEP, 240));
+                        }
+                    }
+                }
+                
+                canvas = matrix.SwapOnVsync(canvas);
+
+                // force 30 FPS
+                var elapsed= stopwatch.ElapsedMilliseconds;
+                if (elapsed < 33)
+                {
+                    Thread.Sleep(33 - (int)elapsed);
+                }
+            }
+
+            return 0;
+        }
+
+        class Point
+        {
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public int x;
+            public int y;
+            public bool recycled;
+        }
+    }
+}
