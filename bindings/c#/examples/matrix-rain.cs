b1bc4f448d4c1dd5d78425aa23e76c15a7cae397
viktor1234678 <viktorvondombrowski@gmail.com>
2021-05-07 15:45:41 +0200

    Update matrix-rain.cs

--- bindings/c#/examples/matrix-rain.cs
+++ bindings/c#/examples/matrix-rain.cs
@@ -58,7 +58,7 @@ namespace matrix_rain
 
                         for (var i=0; i< MAX_HEIGHT; i++)
                         {
-                            canvas.SetPixel(point.x, point.y - i, new Color(point.x, point.y - i * COLOR_STEP, 240));
+                            canvas.SetPixel(point.x, point.y - i, new Color(point.x - i * COLOR_STEP, point.y, 240));
                         }
                     }
                 }
